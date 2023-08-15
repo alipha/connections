d51133e819cf5f0d308d8d0b130dbc833c4aa681
Kevin Spinar <spinarkm@gmail.com>
2023-08-15 18:28:13 -0500

    add custom puzzles 2 & 3

--- custom.js
+++ custom.js
@@ -24,5 +24,7 @@ var customPuzzles =
         ["WOUND", "SAW", "BONE", "KEY"],
         ["BANG", "MONGER", "NAIL", "BOW"]
     ]
-}];
-
+},
+	{"groups":{"SHOE BRANDS":{"level":0,"members":["NIKE","ADIDAS","CONVERSE","VANS"]},"TWO-WHEELED VEHICLES":{"level":1,"members":["BICYCLE","SCOOTER","SEGWAY","MOTORCYCLE"]},"CLASSICAL ELEMENTS":{"level":2,"members":["WATER","EARTH","FIRE","AIR"]},"FIRE _____":{"level":3,"members":["TRUCK","BALL","WOOD","FLY"]}},"startingGroups":[["ADIDAS","SCOOTER","AIR","FLY"],["FIRE","VANS","CONVERSE","WATER"],["BICYCLE","WOOD","EARTH","BALL"],["MOTORCYCLE","TRUCK","NIKE","SEGWAY"]]},
+	{"groups":{"____ HIGH (PHONETICALLY)":{"level":0,"members":["WAIST","SKY","BALI","CLONE"]},"(PYTHON) DATA TYPES":{"level":1,"members":["LIST","ARRAY","STRING","TUPLE"]},"JUNK DRAWER ITEMS":{"level":2,"members":["BATTERY","TAPE","SCISSOR","SCREWDRIVER"]},"TENNIS TERMS":{"level":3,"members":["LOVE","SET","ACE","FAULT"]}},"startingGroups":[["WAIST","TUPLE","ARRAY","CLONE"],["TAPE","SCREWDRIVER","SET","BATTERY"],["FAULT","BALI","LOVE","SKY"],["LIST","ACE","SCISSOR","STRING"]]}
+];
